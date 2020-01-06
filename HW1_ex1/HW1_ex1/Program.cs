d201e9524cacfa4d6357cf32f07ef4db0c787324
bishesh1998 <50055489+bishesh1998@users.noreply.github.com>
2020-01-06 14:18:42 -0800

    Add files via upload
    HW1 Completed

---
+++ HW1_ex1/HW1_ex1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW1_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There");
+            string username = System.Environment.UserName;
+            Console.WriteLine("I see you're known to the system as {0}",username);
+            Console.Write("What do you prefer to be called ? : ");
+            username = Console.ReadLine();
+            //int age;
+            Console.Write("OK {0}. How old are you ? : ",username);
+            string age;
+            age = Console.ReadLine();
+            int value = Convert.ToInt32(age);
+
+            Console.Clear();
+
+            //Checking the condition of minority
+            if (value > 18)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            Console.WriteLine("Hello {0}",username);
+
+            if (value < 18)
+            {
+                string message = "DANGER " + username + " ! DANGER! ";
+                string title = "DANGER";
+                Console.WriteLine(message, title);
+                MessageBox.Show(message, title);
+                Console.Beep();
+            }
+        }
+    }
+}
